0bb802e73787bc1fd3108f309b4160cb6b74ff77
Joona Pullinen <pulline.joona@gmail.com>
2021-11-06 19:43:35 +0200

    Fix post

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ mod models;
 
 #[macro_use] extern crate diesel;
 
+use std::fs;
 use std::fs::File;
 use std::sync::Mutex;
 
 use rocket::*;
+use rocket::request::Form;
 use rocket::response::Redirect;
 use rocket::response::content::Html;
 
@@ -43,13 +45,13 @@ impl App {
 	fn uploads_dir(file_id: &Uuid) -> String {
 		Self::call_with_uuid(file_id, |file_id| format!("uploads/{}", file_id))
 	}
-	fn upload(&self, data: Data) -> Result<Uuid> {
+	fn upload(&self, data: &[u8]) -> Result<Uuid> {
 		let post_id = Uuid::new_v4();
 		let file_id = Uuid::new_v4();
 
 		let path = Self::uploads_dir(&file_id);
 
-		data.stream_to_file(path)?;
+		fs::write(path, data)?;
 
 		let _ = Self::call_with_uuid(&post_id, |post_id| {
 			Self::call_with_uuid(&file_id, |file_id| {
@@ -79,16 +81,19 @@ impl App {
 	}
 }
 
-#[post("/", data = "<paste>")]
-fn upload(db: State<Mutex<App>>, paste: Data) -> Result<Redirect> {
-	println!("{}", String::from_utf8(paste.peek().to_vec())?);
+#[derive(FromForm)]
+struct Paste {
+	paste: String,
+}
 
+#[post("/", data="<paste>")]
+fn upload(db: State<Mutex<App>>, paste: Form<Paste>) -> Result<Redirect> {
 	let lock = db.lock().unwrap();
 
 	let mut buffer = Uuid::encode_buffer();
 
 	let post_id = {
-		let post_id = lock.upload(paste)?;
+		let post_id = lock.upload(paste.paste.as_bytes())?;
 
 		post_id
 			.to_hyphenated()
@@ -110,7 +115,8 @@ fn retrieve(db: State<Mutex<App>>, post_id: String) -> Result<File> {
 fn main() -> Result<()> {
     rocket::ignite()
 		.manage(Mutex::new(App::new("posts.db")?))
-		.mount("/", routes![index, upload, retrieve])
+		.mount("/", routes![index, retrieve, upload])
+		.mount("/upload", routes![upload])
 		.launch();
 
 	Ok(())
